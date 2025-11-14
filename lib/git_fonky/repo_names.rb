dd1b36eaf1332382b964e4c9cc5f51a234b1460d
Collin Jilbert <cjilbert504@gmail.com>
2025-11-14 16:28:05 -0600

    Better handling of the parsing of GFONK_REPOS env var

--- lib/git_fonky/repo_names.rb
+++ lib/git_fonky/repo_names.rb
@@ -1,3 +1,11 @@
 module GitFonky
-  REPO_NAMES = %w[gr hatchbox-v2 jumpstart-marketing jumpstart-pro-rails noticed pay]
+
+  begin
+    REPO_NAMES = ENV["GFONK_REPOS"].split(",")
+  rescue NoMethodError
+    puts "The $GFONK_REPOS environment variable is not set."
+    puts "Please set the variable to point to a string list of repository names separated only by commas (NO SPACES):"
+    puts "export GFONK_REPOS='repo1,repo2,repo3'"
+    exit 1
+  end
 end
