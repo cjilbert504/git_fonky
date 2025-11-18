5915a3dabe02ec87d33b038f4e3c44bcd7bf118a
Collin Jilbert <cjilbert504@gmail.com>
2025-11-18 10:15:42 -0600

    rename/update test

--- test/test_git_fonky_repositories.rb
+++ test/test_git_fonky_repositories.rb
@@ -3,11 +3,11 @@
 require "test_helper"
 
 class TestGitFonkyRepositories < Minitest::Test
-  ENV["GFONK_REPOS"] = "repo1,repo2:staging"
+  def test_parse_env
+    ENV["GFONK_REPOS"] = "repo1,repo2:staging"
 
-  def test_parsing_env_var_returns_a_hash_where_key_is_repo_name_and_value_is_optional_branch_name_or_nil
-    repo_hash = GitFonky::Repositories.parse_env
+    repo_hash = GitFonky::Parser.parse_env
 
-    assert_equal({repo1: nil, repo2: "staging"}, repo_hash)
+    assert_equal({repo1: nil, repo2: "staging"}, repo_hash.config)
   end
 end
